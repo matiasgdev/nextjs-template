fd1853e7176c527642aa30831f5a593bbc12293b
Matias Navarro <matiasg.developer@gmail.com>
2023-08-28 15:49:27 -0300

    fix: import test

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,5 +1,5 @@
 import {render, screen} from '@testing-library/react'
-import Home from '../pages'
+import Home from '../../app/page'
 
 describe('home', () => {
   test('should render title', () => {
